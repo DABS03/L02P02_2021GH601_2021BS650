bacb403e64ca382013ca13c62b3547a0e5a8515a
DiegoBurgos <103340654+DABS03@users.noreply.github.com>
2024-04-20 08:59:33 -0600

    Models completo

---
+++ L02P02_2021GH601_2021BS650/Models/autores.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P02_2021GH601_2021BS650.Models
+{
+    public class autores
+    {
+        [Key]
+        public int id { get; set; }
+        public string autor { get; set; }
+    }
+}
---
+++ L02P02_2021GH601_2021BS650/Models/categorias.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P02_2021GH601_2021BS650.Models
+{
+    public class categorias
+    {
+        [Key]
+        public int id { get; set; }
+        public string categoria { get; set; }
+    }
+}
