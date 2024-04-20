4a18db069f73da9c766280b7ca2317429684687c
DiegoBurgos <103340654+DABS03@users.noreply.github.com>
2024-04-20 08:42:34 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021GH601_2021BS650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021GH601_2021BS650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
